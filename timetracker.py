7b79aee247ed0e5d7e8c26224b6ab074210672fa
Skiby7 <l.scoppitto@studenti.unipi.it>
2021-06-01 19:36:00 +0200

    improved usability

--- timetracker.py
+++ timetracker.py
@@ -4,9 +4,11 @@ from sys import argv, exit
 import os
 import signal
 import re
+import enquiries
+
 path = os.path.dirname(os.path.realpath(__file__)) + "/Projects"
 current_project = None
-
+removing = False
 class bcolors:
 	MAGENTA = '\033[95m'
 	BLUE = '\033[94m'
@@ -19,24 +21,34 @@ class bcolors:
 	ENDC = '\033[0m'
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
+	CLEAR = '\033[2J\033[H'
 
 
 def signal_handler(sig, frame):
-	print('\n\nExiting...')
-	if current_project is not None:
-		current_project.close()
-	exit(0)
+	global removing
+	if not removing:
+		print('\n\nExiting...')
+		if current_project is not None:
+			current_project.close()
+		exit(0)
    
 def print_history():
 	global current_project
+	print()
 	current_project.seek(0)
-	i = 0
-	for line in reversed(current_project.readlines()):
-		print_timestamp()
-		print(re.sub('\[[^\]\[]+?\]', '', str(line)))
-		i += 1
-		if i > 10:
-			break
+	for line in current_project.readlines():
+		if 'TODO' not in line:
+			print(line);
+	input(bcolors.BOLD + 'Press enter to continue' + bcolors.ENDC)
+	print(bcolors.CLEAR)
+	
+def print_todo():
+	global current_project
+	print()
+	current_project.seek(0)
+	for line in current_project.readlines():
+		if 'TODO'  in line:
+			print(line);
 
 def open_project(name):
 	f = open(name, "a+")
@@ -45,12 +57,50 @@ def open_project(name):
 def list_projects():
 	return os.listdir(path)
 
-def print_timestamp():
-	print("[ "+ bcolors.BRIGHT_ORANGE + datetime.datetime.now().strftime("%d-%m-%Y %H:%M") + bcolors.ENDC + " ]", end=" ")
+
+
+def add_line(project, line):
+	if "/todo" in line:
+		line = re.sub('/todo', '', line)
+		line = "[ "+ bcolors.GREEN + "TODO" + bcolors.ENDC + " ] " + line + "\n"
+		project.write(line)
+
+	else:
+		line = "[ "+ bcolors.YELLOW + datetime.datetime.now().strftime("%d-%m-%Y %H:%M") + bcolors.ENDC + " ] " + line + "\n"
+		project.write(line)
+
+def rm_todo():
+	global current_project, removing, path
+	pj_name = current_project.name
+	removing = True
+	current_project.seek(0)
+	lines = current_project.readlines()
+	todos = []
+	for line in lines:
+		if "TODO" in line:
+			todos.append(line)
+	if not todos:
+		print(bcolors.GREEN + "Nothing to do! Take a nap!" + bcolors.ENDC)
+		return
+	choice = enquiries.choose('Select todo:', todos)
+	for line in lines:
+		if line == choice:
+			lines.remove(line)
+	current_project.close()
+	current_project = open(path + "/" + pj_name, "w")
+	for line in lines:
+		current_project.write(line)
+	current_project.close
+	current_project = open_project(pj_name)
+	removing = False
+		
+
 
 def main():
-	global current_project
+	global current_project, window
+	
 	signal.signal(signal.SIGINT, signal_handler)
+	print(bcolors.CLEAR)
 	try:
 		os.chdir(path=path)
 	except FileNotFoundError:
@@ -66,7 +116,9 @@ def main():
 		to_open = input(bcolors.GREEN + "Choose project to open or create a new one -> [" + str_.join([str(elem) for elem in list_projects()]) + "]: " + bcolors.ENDC)
 		current_project = open_project(to_open)
 	while True:
+		print_todo()
 		input_ = input(bcolors.CYAN + current_project.name + "> " + bcolors.ENDC)
+		print(bcolors.CLEAR, end='')
 		if input_ == "/exit":
 			current_project.close()
 			break
@@ -76,8 +128,14 @@ def main():
 			to_open = input(bcolors.GREEN + "Choose project to open or create a new one -> [" + str_.join([str(elem) for elem in list_projects()]) + "]: " + bcolors.ENDC)
 			current_project = open_project(to_open)
 			continue
-		current_project.write("[ "+ datetime.datetime.now().strftime("%d-%m-%Y %H:%M") + " ] " + input_ + "\n")
-		print_history()
+		if input_ == "/done":
+			rm_todo()
+			continue
+		if input_ == "/hist":
+			print_history()
+			continue
+		add_line(current_project, input_)
+		
 
 if __name__ == "__main__":
     main()
